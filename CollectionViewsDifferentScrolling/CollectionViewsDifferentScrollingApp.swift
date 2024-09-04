7d89967a7ea8c59f07e0443f5abaf5b8784cf93e
Anna Panova <annapanovadev@gmail.com>
2024-09-04 13:21:22 +0200

    Initial Commit

---
+++ CollectionViewsDifferentScrolling/CollectionViewsDifferentScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewsDifferentScrollingApp.swift
+//  CollectionViewsDifferentScrolling
+//
+//  Created by Anna Panova on 04.09.24.
+//
+
+import SwiftUI
+
+@main
+struct CollectionViewsDifferentScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
